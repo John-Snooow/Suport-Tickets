5cb969906a0889a78acde966a1287dd7c4acee61
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-08 22:59:22 -0300

    Criando o middleware para obter o body
    Neste trecho, é ensinado como criar um middleware em Node.js para manipular o conteúdo das requisições. É criado um arquivo chamado jsonHandler.js, onde é exportada uma função assíncrona que lida com o corpo das requisições, convertendo para JSON. É mostrado como testar o middleware utilizando o Insomnia para enviar uma requisição POST e verificar o conteúdo recebido. O middleware é essencial para processar e manipular dados enviados nas requisições HTTP.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
 
+import http from 'node:http'
+
+import { jsonHandler } from './middlewares/jsonHandler.js'
+
+async function listener(resqust, response) {
+    await jsonHandler(resqust, response)
+    console.log(resqust.body)
+}
+
+http.createServer(listener).listen(3333)//cria um servidor e passa uma função que vai ser executada toda vez que o servidor receber uma requisição
