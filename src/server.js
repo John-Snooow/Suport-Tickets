cfcc1acf66ba5c2c5cd9baf5bd68cab853f4de88
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-04-09 22:00:59 -0300

    Middleware de rotas
    Criar um middleware para lidar com rotas em uma aplicação.

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@
 import http from 'node:http'
 
 import { jsonHandler } from './middlewares/jsonHandler.js'
+import { routeHandler } from './middlewares/routeHandler.js'
 
 async function listener(resqust, response) {
     await jsonHandler(resqust, response)
-    console.log(resqust.body)
+    routeHandler(resqust, response)
 }
 
 http.createServer(listener).listen(3333)//cria um servidor e passa uma função que vai ser executada toda vez que o servidor receber uma requisição
